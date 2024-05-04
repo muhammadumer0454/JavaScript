c4445869f69719a3c186672c361aebc9b78df84c
Umer <156834998+muhammadumer0454@users.noreply.github.com>
2024-05-04 11:01:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+/*
+
+Variables:-
+
+var:- Can be re-declared and be updated. (Not appreciated to use.)
+
+let:- Cannot be re-declared but can be updated.
+
+const:- Nither can be updated nor be re-declared the value gets constant.
+
+
+
+Loops: For Loop & While Loop:-
+for (var a = 1; a < 10; a++) {
+    console.log(a)
+   }
+
+   const array = [5, "name", true];
+
+
+
+for(a = 0; a < array.length; a++){
+    console.log(array[a]);
+} 
+*/
+
+/*
+
+let company = ["employee-1","employee-2","employee-3"];
+
+let a = 0;
+
+while(a <  company.length ){
+    console.log(company[a])
+    a++
+};
+
+*/
+
+/* 
+
+for(let table = 1; table < 11; table++){
+    console.log("2 x " + table + " = "  + 2 * table ) 
+ } 
+
+ */
+
+ /*
+
+let three_table = 1;
+
+while( three_table < 11){
+    console.log( "3 x " + three_table + " = " + 3 * three_table)
+    three_table++
+};
+
+*/
+
+/*
+
+let three_table = 1;
+
+while( three_table < 11 ){
+    console.log( "3 x " + three_table + " = " + 3 * three_table)
+    three_table++
+};
+
+*/
+
+/*
+const array = ["one", 2 , "three", 4, "five", 6 , "seven", 8];
+
+for(i = 0; i < array.length; i++){
+    console.log(array[i])
+};
+
+*/
